0816a53ac3eb5ee05b9fd10ce9a88df9fb95d53a
Daniel Somerfield <dsomerfi@thoughtworks.com>
2015-01-03 10:47:51 -0800

    Added better testing assertions for return code. Better harness for http.

--- src/test/groovy/helloService/HelloServiceUATest.groovy
+++ src/test/groovy/helloService/HelloServiceUATest.groovy
@@ -8,10 +8,19 @@ import static org.junit.Assert.assertEquals
 class HelloServiceUATest {
     @Test
     def void testHelloServiceEndpoint() {
-        def jsonResult = doGetRequest("/hello?name=Daniel")
-        assertEquals(
-                "Hello, Daniel!",
-                jsonResult.message
-        )
+        doGetRequest("/hello/?name=Daniel") {
+            assertEquals(200, it.httpStatus)
+            assertEquals(
+                    "Hello, Daniel!",
+                    it.json.message
+            )
+        }
+    }
+
+    @Test
+    def void testHelloServiceEndpointRedirect() {
+        doGetRequest("/hello?name=Daniel") {
+            assertEquals(302, it.httpStatus)
+        }
     }
 }
