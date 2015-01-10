f6431508906343ff0a22eaf2f588dcf02160934d
Daniel Somerfield <dsomerfi@thoughtworks.com>
2015-01-10 14:14:55 -0800

    Added smoke test suite.

---
+++ src/test/groovy/helloService/HelloServiceSmokeTest.groovy
@@ -0,0 +1,10 @@
+package helloService
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses([HelloServiceUATest.class])
+class HelloServiceSmokeTest {
+
+}
