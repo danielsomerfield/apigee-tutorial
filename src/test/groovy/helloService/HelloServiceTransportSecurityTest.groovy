51051df0243e91ce566f23c1d9c35bc1773a1d4d
Daniel Somerfield <dsomerfi@thoughtworks.com>
2015-01-30 22:11:40 -0800

    Fix to sec tests.

--- src/test/groovy/helloService/HelloServiceTransportSecurityTest.groovy
+++ src/test/groovy/helloService/HelloServiceTransportSecurityTest.groovy
@@ -1,9 +1,14 @@
 package helloService
+
+import org.hamcrest.CustomMatcher
+import org.hamcrest.Matcher
 import org.junit.Test
 import testUtils.HttpBuilder
 
+import java.util.function.Consumer
+
 import static org.junit.Assert.assertEquals
-import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertThat
 import static testUtils.HttpBuilder.Scheme.http
 import static testUtils.HttpBuilder.Scheme.https
 import static testUtils.HttpBuilder.serviceClient
@@ -28,8 +33,23 @@ class HelloServiceTransportSecurityTest {
             port = 80
             scheme = http
         }.execute().then { Optional<HttpBuilder.Response> response->
-            assertFalse(response.isPresent());
+            response.ifPresent(new Consumer<HttpBuilder.Response>() {
+                @Override
+                void accept(final HttpBuilder.Response r) {
+                    assertThat(r.getHttpStatus(), isErrorCode())
+                }
+
+                Matcher<? super Integer> isErrorCode() {
+                    return new CustomMatcher<Integer>() {
+                        @Override
+                        boolean matches(final Object item) {
+                            return item instanceof Integer && item > 299
+                        }
+                    }
+                }
+            })
         }
     }
+
 
 }
